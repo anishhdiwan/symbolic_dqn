55d65237eb019e267e6b8b78b2d8ea010f7ea0f5
anishdiwan <anishhdiwan@gmail.com>
2023-06-10 14:47:47 +0200

    Add pre-order traversal and Node method, class; clean up main.py

--- DQN/main.py
+++ DQN/main.py
@@ -38,8 +38,7 @@ save_path = f"saved_models/frame_stack:{FRAME_STACK}_|batch_size:{BATCH_SIZE}_|g
 writer = SummaryWriter(log_dir=logdir)
 
 # Creating the environment (this may take a few minutes) and setting up the data sampling iterator
-env = gym.make('MineRLTreechop-v0')
-print("Gym.make done")
+env = model.Environment()
 
 replay_memory = model.ReplayMemory(5000)
 print("Replay memory & demo replay memory initialized")
@@ -54,5 +53,60 @@ target_net.load_state_dict(policy_net.state_dict())
 
 # Defining the loss function and optimizer
 optimizer = optim.Adam(policy_net.parameters(), lr=LR, weight_decay=1e-5) # Weight decay is L2 regularization
-dqfd_loss = model.DQfD_Loss()
+dqn_loss = model.DQN_Loss()
 
+
+
+
+# Main function
+for i_episode in range(num_episodes):
+    # Initialize the environment and get it's state
+    state = env.reset()
+    # Metrics
+    episode_return = 0
+    episode_steps = 0
+    loop = tqdm(range(num_steps))
+    for t in loop:
+        loop.set_description(f"Episode {i_episode} Steps | CPU {psutil.cpu_percent()} | RAM {psutil.virtual_memory().percent}")   
+        action = model.select_action(torch.reshape(torch.tensor(state, dtype=torch.float32), (1,-1)), EPS, policy_net)
+
+        
+        next_state, reward, done = env.step(action)
+        replay_memory.append(state, action, reward, next_state)
+
+        # Move to the next state
+        state = next_state
+
+        # Perform one step of the optimization (on the policy network)
+        loss = model.optimize_model(optimizer, policy_net, target_net, replay_memory, dqn_loss, BATCH_SIZE=BATCH_SIZE, GAMMA=GAMMA)       
+        
+        # Logging step level metrics
+        episode_return += reward
+        episode_steps = t
+        writer.add_scalar("Loss vs Total Steps (all episodes)", loss, total_steps)
+        total_steps += 1
+
+        # Soft update of the target network's weights
+        # θ′ ← τ θ + (1 −τ )θ′
+        target_net_state_dict = target_net.state_dict()
+        policy_net_state_dict = policy_net.state_dict()
+        for key in policy_net_state_dict:
+            target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
+        target_net.load_state_dict(target_net_state_dict)
+        # print("Completed one step of soft update")
+        
+        if (total_steps % save_checkpoint) == 0:
+            torch.save(policy_net.state_dict(), save_path)
+
+        if done:
+            break
+        # print("--------------")
+
+    # Logging episode level metrics
+    writer.add_scalar("Num Steps vs Episode", episode_steps, i_episode)
+    writer.add_scalar("Total Episode Return vs Episode", episode_return, i_episode)
+
+writer.close()
+torch.save(policy_net.state_dict(), save_path)
+
+print('Complete')
--- DQN/model.py
+++ DQN/model.py
@@ -8,19 +8,43 @@ import torch.optim as optim
 import torch.nn.functional as F
 
 
-class SymbolicEq():
+# A class that represents an individual node
+# in a Binary Tree
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+
+# A function to do preorder tree traversal
+def printPreorder(root):
+
+    if root:
+
+        # First print the data of node
+        print(root.val, end=" "),
+
+        # Then recur on left child
+        printPreorder(root.left)
+
+        # Finally recur on right child
+        printPreorder(root.right)
+
+
+class EquationTree():
 	'''
-	The SymbolcEq class is used to instantiate symbolic equation objects. These objects act as states in the first MDP. 
+	The EquationTree class is used to instantiate symbolic equation objects. These objects act as states in the first MDP. 
 	'''
 
 	def __init__(self):
-		self.pre_order_trav = [0,0,0]
+		self.pre_order_trav = None
 
 	def update(self, action):
 		# Update the pre-order traversal of the current state with the performed action 
 
 	def evaluate(self):
-		# Evaluate the current symbolic eq with the next_env's state to get action values for the next_env
+		# Evaluate the current symbolic eq with the main_env's state to get action values for the main_env
 
 class Environment:
 	'''
@@ -30,20 +54,23 @@ class Environment:
 	is the actual gym env. 
 	'''
 
-	def __init__(self, next_env):
-		self.state = SymbolicEq()
-		self.next_env = next_env
+	def __init__(self, main_env):
+        # main_env is an instance of the OpenAI gym environment (in this case, lunar lander)
+		self.state = EquationTree()
+		self.main_env = main_env
 		self.reward = None
-		self.done = done
+		self.done = None
+        self.main_env_state = None
 
 	def reset(self):
-		self.state = [0,0,0]
+		self.state = EquationTree()
 		return self.state
 
 	def step(self, action):
 		self.state = self.state.update(action)
-		next_env_action = self.state.evaluate()
-		reward, done = next_env.step(next_env_action)
+        state_eval = self.state.evaluate(self.main_env_state)
+		main_env_action = select_action(state_eval)
+		reward, done = main_env.step(main_env_action)
 		self.reward = reward
 		self.done = done
 		return self.state, self.reward, self.done
@@ -109,7 +136,7 @@ class DQN(nn.Module):
 # Defining the temporal difference loss function
 class DQN_Loss(nn.Module):
     def __init__(self):
-        super(DQfD_Loss, self).__init__()
+        super(DQN_Loss, self).__init__()
 
 
     def forward(self, policy_net, target_net, states, actions, rewards, next_states, dones, GAMMA, large_margin=True):
@@ -120,19 +147,7 @@ class DQN_Loss(nn.Module):
         targets = rewards + GAMMA * next_state_max # torch.max(target_net(next_states), dim=1).values
         values = policy_net(states).gather(1,actions.view(-1,1)).view(-1,)
 
-        if large_margin == True:
-            # large margin loss
-            lm1 = torch.max(policy_net(states), dim=1).values # Q value of the agent's actions as per its current policy
-            lm2 = torch.eq(torch.argmax(policy_net(states), dim=1), actions).float() # Margin function: 0 if agent's action is the same as the expert's action 1 otherwise
-            with torch.no_grad():
-                lm3 = target_net(states).gather(1, actions.view(-1,1)).view(-1,) # Q value of the expert's action as per the target net (similar to why we use a frozen target)
-
-            large_margin_loss = torch.mean(lm1+lm2+lm3)        
-
-            return F.mse_loss(values, targets) + large_margin_loss
-            
-        else:
-            return F.mse_loss(values, targets)
+        return F.mse_loss(values, targets)
 
 
 # Defining epsilon greedy action selection
@@ -142,7 +157,7 @@ def select_action(state, EPS, policy_net):
     if sample > EPS:
         # print("Exploiting")
         with torch.no_grad():
-            return torch.argmax(policy_net(state, for_optimization=False), dim=1).item()
+            return torch.argmax(policy_net(state, for_optimization=True), dim=1).item()
     else:
         # print("Exploring")
         return action_names[random.choice(list(action_names.keys()))]
@@ -150,54 +165,43 @@ def select_action(state, EPS, policy_net):
 
 
 # Defining the optimization for the Q-network
-def optimize_model(optimizer, policy_net, target_net, replay_memory, demo_replay_memory, dqfd_loss, BATCH_SIZE = 32, BETA = 0.5, GAMMA=0.99):
+def optimize_model(optimizer, policy_net, target_net, replay_memory, demo_replay_memory, dqn_loss, BATCH_SIZE = 32, BETA = 0.5, GAMMA=0.99):
     '''
     Optimize the Q-network either using the agent's self-explored replay memory or using demo data. 
     The variable BETA defines the probability of sampling from either one. This will later be replaced by some importance sampling factor
     '''
 
-    sample = random.random()
-    if sample > BETA:
-        # print("Sampling from demo replay memory")
-        batch_states, batch_actions, batch_rewards, batch_next_states, batch_dones = sample_demo_batch(demo_replay_memory, BATCH_SIZE, grayscale=True)
-        batch_states, batch_next_states, batch_rewards, batch_actions = batch_states.to(device), batch_next_states.to(device), batch_rewards.to(device), batch_actions.to(device)
-        loss = dqfd_loss(policy_net, target_net, batch_states, batch_actions, batch_rewards, batch_next_states, batch_dones, GAMMA, large_margin=True)
+    # print("Sampling from agent's replay memory")
+    batch_transitions = replay_memory.sample(BATCH_SIZE)
 
-        # print(f"Loss: {loss}")
+    batch_states = []
+    batch_actions = []
+    batch_rewards = []
+    batch_next_states = []
+    batch_dones = []
 
+    for i in range(BATCH_SIZE):
+        batch_states.append(batch_transitions[i].state)
+        batch_next_states.append(batch_transitions[i].next_state)
+        batch_rewards.append(batch_transitions[i].reward)
+        batch_actions.append(batch_transitions[i].action)
 
-    else:
-        # print("Sampling from agent's replay memory")
-        batch_transitions = replay_memory.sample(BATCH_SIZE)
+    # batch_states = torch.reshape(torch.tensor(np.array(batch_states), dtype=torch.float32, requires_grad=True), (BATCH_SIZE,-1))
+    batch_states = torch.tensor(np.array(batch_states), dtype=torch.float32, requires_grad=True)
+    # batch_next_states = torch.reshape(torch.tensor(np.array(batch_next_states), dtype=torch.float32, requires_grad=True), (BATCH_SIZE,-1))
+    batch_next_states = torch.tensor(np.array(batch_next_states), dtype=torch.float32, requires_grad=True)
+    batch_actions = torch.tensor(np.array(batch_actions))
+    batch_rewards = torch.tensor(np.array(batch_rewards), dtype=torch.float32, requires_grad=True)
+    batch_dones = torch.tensor(np.array(batch_dones))
 
-        batch_states = []
-        batch_actions = []
-        batch_rewards = []
-        batch_next_states = []
-        batch_dones = []
-
-        for i in range(BATCH_SIZE):
-            batch_states.append(batch_transitions[i].state)
-            batch_next_states.append(batch_transitions[i].next_state)
-            batch_rewards.append(batch_transitions[i].reward)
-            batch_actions.append(batch_transitions[i].action)
-
-        # batch_states = torch.reshape(torch.tensor(np.array(batch_states), dtype=torch.float32, requires_grad=True), (BATCH_SIZE,-1))
-        batch_states = torch.tensor(np.array(batch_states), dtype=torch.float32, requires_grad=True)
-        # batch_next_states = torch.reshape(torch.tensor(np.array(batch_next_states), dtype=torch.float32, requires_grad=True), (BATCH_SIZE,-1))
-        batch_next_states = torch.tensor(np.array(batch_next_states), dtype=torch.float32, requires_grad=True)
-        batch_actions = torch.tensor(np.array(batch_actions))
-        batch_rewards = torch.tensor(np.array(batch_rewards), dtype=torch.float32, requires_grad=True)
-        batch_dones = torch.tensor(np.array(batch_dones))
-
-        # print(batch_states.shape)
-        # print(batch_next_states.shape)
-        # print(batch_actions.shape)
-        # print(batch_rewards.shape)
-        # print(batch_dones.shape)
-        batch_states, batch_next_states, batch_rewards, batch_actions = batch_states.to(device), batch_next_states.to(device), batch_rewards.to(device), batch_actions.to(device)
-        loss = dqfd_loss(policy_net, target_net, batch_states, batch_actions, batch_rewards, batch_next_states, batch_dones, GAMMA, large_margin=False)
-        # print(f"Loss: {loss}")
+    # print(batch_states.shape)
+    # print(batch_next_states.shape)
+    # print(batch_actions.shape)
+    # print(batch_rewards.shape)
+    # print(batch_dones.shape)
+    batch_states, batch_next_states, batch_rewards, batch_actions = batch_states.to(device), batch_next_states.to(device), batch_rewards.to(device), batch_actions.to(device)
+    loss = dqn_loss(policy_net, target_net, batch_states, batch_actions, batch_rewards, batch_next_states, batch_dones, GAMMA, large_margin=False)
+    # print(f"Loss: {loss}")
 
     # Optimize the model
     optimizer.zero_grad()
