Fix exp overflow error; fix nan probs error

symbolic_dqn/main.py
@@ -23,11 +23,11 @@ device = torch.device("cpu")
 # TAU is the update rate of the target network
 # LR is the learning rate of the optimizer
 BATCH_SIZE = 32
-GAMMA = 0.9
-EPS = 0.05
+GAMMA = 0.99
+EPS = 0.01
 TAU = 0.005
 LR = 1e-4
-num_episodes = 200
+num_episodes = 100
 num_steps = 100
 save_checkpoint = 50 # save the model after these many steps
 RUN_NAME = "HP_combo_1"
@@ -96,7 +96,7 @@ for i_episode in range(num_episodes):
         - Individual replay buffers are filled with the state, action (operation addition), reward (from deep copies), and next state if those trees were not already full
         - The env is done if no more state additions are possible, or if the main_env is done.
         '''
-        next_states, rewards, done, tree_full_before_step, main_env_steps = env.step(actions)
+        next_states, rewards, done, tree_full_before_step = env.step(actions)
 
         for i in range(len(replay_memories)):
             if not tree_full_before_step[i]:
@@ -116,7 +116,7 @@ for i_episode in range(num_episodes):
         for i in range(len(losses)):
            writer.add_scalar(f"Loss: Policy Net {i} vs Total Steps (across all episodes)", losses[i], total_steps)
         total_steps += 1
-        writer.add_scalar(f"Main env steps per multi tree env step", main_env_steps, total_steps)
+        # writer.add_scalar(f"Main env steps per multi tree env step", main_env_steps, total_steps)
 
         # Soft update of the target network's weights
         # θ′ ← τ θ + (1 −τ )θ′

symbolic_dqn/model.py
@@ -63,7 +63,7 @@ class Environment:
 				rewards = np.array([0,0,0,0])
 
 				# Stepping through k steps of the main env to evaluate the current multi-tree
-				count = 0
+				# count = 0
 				for _ in range(self.main_env_steps_per_first_env_step):
 					if not self.done:
 						state_eval = self.state.evaluate(self.main_env_state)
@@ -75,15 +75,15 @@ class Environment:
 							observation = self.main_env.reset()[0]
 						self.main_env_state = torch.from_numpy(observation.reshape((1,-1))).float()
 						rewards[main_env_action] += reward
-						count += 1
+						# count += 1
 
 				# print(count)
-				rewards += count
+				# rewards += count
 
 				if not False in self.tree_full:
 					self.done = True
 
-				return self.state.vectorise_preorder_trav(), rewards, self.done, tree_full_before_update, count
+				return self.state.vectorise_preorder_trav(), rewards, self.done, tree_full_before_update
 			
 			else:
 				self.done = True
@@ -202,10 +202,18 @@ def select_action(states, EPS, policy_nets, node_instances):
 # Defining softmax actions selection for the main environment
 def select_main_env_action(state_eval):
 	actions = [0,1,2,3]
-	probabilities = F.softmax(state_eval, dim=0)
-	probabilities = probabilities.detach().numpy().flatten()
-	probabilities /= probabilities.sum()	
+	# probabilities = F.softmax((state_eval - state_eval.max), dim=0)
+	# probabilities = probabilities.detach().numpy().flatten()
+	# probabilities /= probabilities.sum()
 
+	# return np.random.choice(actions, p=probabilities)
+
+	soft = nn.Softmax(dim=-1)
+	probabilities = soft((state_eval/state_eval.max()))
+	probabilities = probabilities.cpu().detach().numpy()[0]
+	if True in np.isnan(probabilities):
+		probabilities = np.array([0.25, 0.25, 0.25, 0.25])
+		
 	return np.random.choice(actions, p=probabilities)
 
 